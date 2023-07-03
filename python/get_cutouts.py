69d558ae7bf7c87eece89180262fa88eb4c30642
Raphael Williams <43833347+swilliams662@users.noreply.github.com>
2023-07-03 17:04:56 -0400

    Update get_cutouts.py

--- python/get_cutouts.py
+++ python/get_cutouts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[9]:
 
 
 import numpy as np
@@ -17,33 +17,60 @@ import astroquery
 from astroquery.ipac.ned import Ned
 import requests
 import csv
+from requests_html import HTMLSession
+from selenium.webdriver import ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#things just for my firefox stuff:
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 
-# In[22]:
+# In[52]:
+
+
+#just for me
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+
+# In[5]:
+
+
+#just for me
+driver = webdriver.Firefox()
+
+
+# In[10]:
 
 
 def get_cutouts(file_path, radius):
-    url = 'http://cutouts.cirada.ca/'
-    payload = {'upload': (None, open(file_path, 'rb'), 'text/csv')}
-    r = requests.post(url, files=payload)
-    if r.status_code == 200:
-        print('File upload successful.')
-        options_payload = {
-            'display': '2',
-            'options[]': ['VLASS-QL', 'FIRST', 'NVSS'],
-            'radius': str(radius),
-            'group': 'mosaic'
-        }
-        r = requests.post(url, data=options_payload)
-        #print(r.text)
-    else:
-        print('File upload failed. Status code:', r.status_code)
+    driver.get('http://cutouts.cirada.ca/')
+    upload = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="file"]'))
+    )
+    #upload = driver.find_element(By.ID, "id_batch_locations")
+    upload.send_keys(file_path)
+    #upload.submit()
+    checkbox = driver.find_element(By.ID, 'id_surveys_0')
+    checkbox.click()
+    checkbox = driver.find_element(By.ID, 'id_surveys_3') #FIRST
+    checkbox.click()
+    checkbox = driver.find_element(By.ID, 'id_surveys_4') #NVSS
+    checkbox.click()
+    radius = driver.find_element(By.ID, 'id_radius')
+    radius.send_keys(str(radius))
+    mosaic = driver.find_element(By.ID, 'id_groups')
+    mosaic.send_keys('MOSAIC')
+    submit = driver.find_element(By.ID, 'form_submit')
+    submit.click()
 
 
-# In[23]:
+# In[11]:
 
 
-get_cutouts('agnpositions.csv', 2)
+get_cutouts('/Users/summerwilliams/Desktop/blanton/agnpositions.csv',2)
 
 
 # In[ ]:
